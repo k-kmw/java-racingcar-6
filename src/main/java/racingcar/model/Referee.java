25c69fe1c4e61eb5f7665327d626b0af5cb0708f
k-kmw <kimmw2_@naver.com>
2023-12-04 16:09:11 +0900

    refactor: stream 사용하여 메서드 리팩토링

--- src/main/java/racingcar/model/Referee.java
+++ src/main/java/racingcar/model/Referee.java
@@ -5,18 +5,20 @@ import java.util.List;
 
 public class Referee {
     public Cars judge(Cars cars) {
-        int mx = 0;
-        List<Car> winnerCars = new ArrayList<>();
-        for (Car car : cars.getCars()) {
-            if(car.getTrack().length() > mx) {
-                mx = car.getTrack().length();
-            }
-        }
-        for (Car car : cars.getCars()) {
-            if (car.getTrack().length() == mx) {
-                winnerCars.add(car);
-            }
-        }
-        return new Cars(winnerCars);
+        int maxMoveNum = findMaxMoveNum(cars);
+        return getWinnerCars(cars, maxMoveNum);
+    }
+
+    private Cars getWinnerCars(Cars cars, int maxMoveNum) {
+        return new Cars(cars.getCars().stream()
+                .filter(car -> car.getTrack().length() == maxMoveNum)
+                .toList());
+    }
+
+    private int findMaxMoveNum(Cars cars) {
+         return cars.getCars().stream()
+                .mapToInt(car -> car.getTrack().length())
+                .max()
+                .orElse(0);
     }
 }
